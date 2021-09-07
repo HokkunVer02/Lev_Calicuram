3aeeb3237555c8e85d351d43fc72c9ca5aa3e3ed
HokkunVer02 <h.iwami.denden0101@outlook.jp>
2021-09-08 03:02:27 +0900

    記事編集

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Post;
 use App\Http\Requests\PostRequest;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
@@ -23,6 +25,18 @@ class PostController extends Controller
     }
     
     public function store(Post $post, PostRequest $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    
+    public function edit(Post $post)
+    {
+        return view('edit')->with(['post' => $post]);
+    }
+    
+    public function update(Request $request, Post $post)
     {
         $input = $request['post'];
         $post->fill($input)->save();
